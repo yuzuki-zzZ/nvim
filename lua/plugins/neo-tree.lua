f5ee0359adf2799c0e7ebc8c19d5e63151836254
Keaganliu <keagan.liu@developertools.com>
2024-04-19 18:03:46 +0900

    add neo-tree document_symbols source

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,36 +1,45 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    require("neo-tree").setup({
-      filesystem = {
-        follow_current_file = {
-          enabled = true,
-        },
-        window = {
-          mappings = {
-            ["<CR>"] = function(state)
-              require("neo-tree.sources.filesystem.commands").open(state)
-              require("neo-tree").close_all()
-            end,
-            ["S"] = function(state)
-              require("neo-tree.sources.filesystem.commands").open_vsplit(state)
-              require("neo-tree").close_all()
-            end,
-          },
-        },
-      },
-      --window = {
-      -- mappings = {
-      --  ["<leader>"] = {"toggle_node", nowait = false}
-      --}
-      --}
-    })
-    vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
-  end,
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			sources = {
+				"filesystem", -- Assuming 'filesystem' is already a source
+				"buffers",
+				"document_symbols",
+				"git_status",
+			},
+			filesystem = {
+				follow_current_file = {
+					enabled = true,
+				},
+				window = {
+					mappings = {
+						["<CR>"] = function(state)
+							require("neo-tree.sources.filesystem.commands").open(state)
+							require("neo-tree").close_all()
+						end,
+						["S"] = function(state)
+							require("neo-tree.sources.filesystem.commands").open_vsplit(state)
+							require("neo-tree").close_all()
+						end,
+						["<Leader><Space>"] = function(state)
+							require("neo-tree.sources.filesystem.commands").open(state)
+						end,
+					},
+				},
+			},
+			--window = {
+			-- mappings = {
+			--  ["<leader>"] = {"toggle_node", nowait = false}
+			--}
+			--}
+		})
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>", {})
+	end,
 }
