4d4f61ca1e5aee7c381f9288dcf9b2000c94d9b6
yuzuki-zzZ <keaganliu0@gmail.com>
2024-04-21 13:05:18 +0800

    adjust lualine layout

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,36 +9,47 @@ return {
 			sections = {
 				lualine_a = {
 					{
-						"buffers",
-						show_filename_only = true, -- Shows shortened relative path when set to false.
-						hide_filename_extension = false, -- Hide filename extension when set to true.
-						show_modified_status = true, -- Shows indicator when the buffer is modified.
-
-						mode = 4, -- 0: Shows buffer name
-						-- 1: Shows buffer index
-						-- 2: Shows buffer name + buffer index
-						-- 3: Shows buffer number
-						-- 4: Shows buffer name + buffer number
-
-						max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
-						-- it can also be a function that returns
-						-- the value of `max_length` dynamically.
-
-						filetype_names = {
-							TelescopePrompt = "Telescope",
-							dashboard = "Dashboard",
-							packer = "Packer",
-							fzf = "FZF",
-							alpha = "Alpha",
-						},
-
-						symbols = {
-							modified = " ●", -- Text to show when the buffer is modified
-							alternate_file = "#", -- Text to show to identify the alternate file
-							directory = "", -- Text to show when the buffer is a directory
-						},
+						"mode",
 					},
 				},
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+					},
+				},
+				-- lualine_a = {
+				-- {
+				-- 	"buffers",
+				-- 	show_filename_only = true, -- Shows shortened relative path when set to false.
+				-- 	hide_filename_extension = false, -- Hide filename extension when set to true.
+				-- 	show_modified_status = true, -- Shows indicator when the buffer is modified.
+				--
+				-- 	mode = 4, -- 0: Shows buffer name
+				-- 	-- 1: Shows buffer index
+				-- 	-- 2: Shows buffer name + buffer index
+				-- 	-- 3: Shows buffer number
+				-- 	-- 4: Shows buffer name + buffer number
+				--
+				-- 	max_length = vim.o.columns * 2 / 3, -- Maximum width of buffers component,
+				-- 	-- it can also be a function that returns
+				-- 	-- the value of `max_length` dynamically.
+				--
+				-- 	filetype_names = {
+				-- 		TelescopePrompt = "Telescope",
+				-- 		dashboard = "Dashboard",
+				-- 		packer = "Packer",
+				-- 		fzf = "FZF",
+				-- 		alpha = "Alpha",
+				-- 	},
+				--
+				-- 	symbols = {
+				-- 		modified = " ●", -- Text to show when the buffer is modified
+				-- 		alternate_file = "#", -- Text to show to identify the alternate file
+				-- 		directory = "", -- Text to show when the buffer is a directory
+				-- 	},
+				-- },
+				--},
 			},
 		})
 	end,
